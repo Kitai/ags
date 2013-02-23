89d6848012a9d86f629367ed22333507fa486322
Billbis <gdevailly@hotmail.com>
2013-02-23 22:01:36 +0100

    Update Editor/AGS.Editor/Resources/agsdefns.sh
    Corrigé d’après un précédent commit de Kitai, no fusionnable automatiquement.

--- Editor/AGS.Editor/Resources/agsdefns.sh
+++ Editor/AGS.Editor/Resources/agsdefns.sh
@@ -1712,13 +1712,13 @@ managed struct AudioClip {
   import AudioChannel* Play(AudioPriority=SCR_NO_VALUE, RepeatStyle=SCR_NO_VALUE);
   /// Joue la séquence audio en démarrant depuis la position spécifiée.
   import AudioChannel* PlayFrom(int position, AudioPriority=SCR_NO_VALUE, RepeatStyle=SCR_NO_VALUE);
-  /// oue la séquence audio, ou la met à la file afin qu'elle soit jouée plus tard si ce n'est pas possible actuellement. 
+  /// Joue la séquence audio, ou la met à la file si aucun canal n'est libre.
   import AudioChannel* PlayQueued(AudioPriority=SCR_NO_VALUE, RepeatStyle=SCR_NO_VALUE);
-  /// Coupe toutes les diffusions en cours de cette séquence audio.
+  /// Arrête toutes les diffusions en cours de cette séquence audio.
   import void Stop();
   /// Retourne le type de fichier de la séquence audio.
   readonly import attribute AudioFileType FileType;
-  /// Retourne si la séquence audio est disponible sur l'ordinateur du joueur. 
+  /// Vérifie si la séquence audio est disponible sur l'ordinateur du joueur. 
   readonly import attribute bool IsAvailable;
   /// Retourne le type de la séquence audio, telle que définie dans l'éditeur.
   readonly import attribute AudioType Type;
@@ -1734,19 +1734,19 @@ struct System {
 #ifndef STRICT_STRINGS
   readonly char version[10];
 #endif
-  /// Retourne si Caps Lock est actif.
+  /// Retourne si le verouillage majuscules (Caps Lock) est actif.
   readonly import static attribute bool CapsLock;
-  /// Retourne un pointeur vers la chaine audio spécifée.
+  /// Retourne un pointeur vers le canal audio spécifé.
   readonly import static attribute AudioChannel *AudioChannels[];   // $AUTOCOMPLETESTATICONLY$
-  /// Retourne le nombre de chaines audio supportés part AGS.
+  /// Retourne le nombre de canaux audio supportés part AGS.
   readonly import static attribute int  AudioChannelCount;   // $AUTOCOMPLETESTATICONLY$
   /// Retourne la profondeur de couleur à laquelle le jeu est exécuté.
   readonly import static attribute int  ColorDepth;
-  /// Retourne/définit le niveau Gamma du jeu.
+  /// Retourne/Définit le niveau Gamma du jeu.
   import static attribute int  Gamma;
   /// Retourne si le jeu est en mode accélération matérielle.
   readonly import static attribute bool HardwareAcceleration;
-  /// Retourne si Num Lock est activé.
+  /// Retourne si le verrouillage numérique (Num Lock) est activé.
   readonly import static attribute bool NumLock;
   /// Retourne le système d'exploitation sous lequel tourne le jeu actuellement.
   readonly import static attribute eOperatingSystem OperatingSystem;
@@ -1764,9 +1764,9 @@ struct System {
   readonly import static attribute int  ViewportHeight;
   /// Retourne la largeur actuelle de la fenêtre, selon le systéme de coordonnées du jeu
   readonly import static attribute int  ViewportWidth;
-  /// Retourne/définit le volume sonore, de 0 à 100.
+  /// Retourne/Définit le volume sonore, de 0 à 100.
   import static attribute int  Volume;
-  /// Retourne/définit si AGS effectue une synchronisation verticale avant d'afficher chaque image.
+  /// Retourne/Définit si AGS effectue une synchronisation verticale avant d'afficher chaque image.
   import static attribute bool VSync;
   /// Retourne si le jeu tourne actuellement en mode fenêtré.
   readonly import static attribute bool Windowed;
